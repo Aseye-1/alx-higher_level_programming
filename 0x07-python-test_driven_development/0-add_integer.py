f8ec5f6dedd775e33051b0d2417c1b5cd924d9e5
Aseye-1 <aboraseye@gmail.com>
2023-04-14 05:12:31 +0000

    Task 0 :tada:

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
+# 0-add_integer.py
+"""Defines an integer addition function."""
 
-"""
-This module contains a function that adds two integers
-"""
 
 def add_integer(a, b=98):
-    """
-    Adds two integers a and b
+    """Return the integer addition of a and b.
 
-    Args:
-        a (int/float): The first number.
-        b (int/float): The second number. Defaaults to 98.
-
-    Returns:
-        int: The sum of a and b.
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: If a or b are not integers or floats.
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-
-    return int(a) + int(b)
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,8 +1,2 @@
+0x07. Python - Test-driven development
 
-Python - Test-driven development
-What’s an interactive test
-Why tests are important
-How to write Docstrings to create tests
-How to write documentation for each module and function
-What are the basic option flags to create tests
-How to find edge cases
